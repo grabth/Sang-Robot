dbb597245282ac4ce09012abaac658ce27c3c792
Haris Avicenna <75520927+grabth@users.noreply.github.com>
2021-06-17 21:16:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-ScrollReveal({ reset: true });
-ScrollReveal().reveal('.desc', { delay: 200 });
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
