f8d06763ec944affcc73d30ad0ff43784b7a7adb
grabth <harisavicenna@gmail.com>
2021-06-21 11:03:45 +0700

    Changed
    Updated footer and fixed bug

--- js/script.js
+++ js/script.js
@@ -20,11 +20,16 @@ function myFunction() {
   ul = document.getElementById("myMenu");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
-     a = li[i].getElementsByTagName("a")[0];
+     a = li[i].getElementsByTagName("button")[0];
      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
      } else {
         li[i].style.display = "none";
      }
   }
+  }
+
+  function Totop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
